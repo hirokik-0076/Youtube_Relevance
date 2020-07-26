775d55337c3376083964cdca762fa3c448fad1ff
Hiroki Kinoshita <hiroki.kinoshita@sun-asterisk.com>
2020-07-26 18:56:43 +0900

    add Drawer

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,10 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
 import {
   AppBar,
   Toolbar,
   Typography,
   IconButton,
+  Drawer
 } from '@material-ui/core';
 import MenuIcon from '@material-ui/icons/Menu';
 
@@ -24,19 +25,39 @@ const useStyles = makeStyles((theme:Theme) =>
 
 const App:React.FC = () => {
   const classes = useStyles();
+  const [isOpen, setIsOpen] = useState(false);
+  const toggleDrawer = (open: boolean) => (
+    event: React.KeyboardEvent | React.MouseEvent,
+  ) => {
+    if (
+      event.type === 'keydown' &&
+      ((event as React.KeyboardEvent).key === 'Tab' ||
+        (event as React.KeyboardEvent).key === 'Shift')
+    ) {
+      return;
+    }
+
+    setIsOpen(open);
+  };
+
 
   return(
-    <div className={classes.root}>
-      <AppBar position="static">
-        <Toolbar>
-          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h6" className={classes.title}>
-            Youtube関連度ランキングサイト
-          </Typography>
-        </Toolbar>
-      </AppBar>
+    <div>
+      <div className={classes.root}>
+        <AppBar position="static">
+          <Toolbar>
+            <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu" onClick={toggleDrawer(true)}>
+              <MenuIcon />
+            </IconButton>
+            <Typography variant="h6" className={classes.title}>
+              Youtube関連度ランキングサイト
+            </Typography>
+          </Toolbar>
+        </AppBar>
+      </div>
+      <Drawer open={isOpen} onClose={toggleDrawer(false)}>
+        テストテストテストテストテストテスト
+      </Drawer>
     </div>
   );
 }
